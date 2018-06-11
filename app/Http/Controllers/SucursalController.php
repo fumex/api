105ce4c16cdf70dab04968a62ed6dba288f35a09
fumex <grego07h@gmail.com>
2018-06-11 15:47:52 -0500

    sucursal  v.1

--- app/Http/Controllers/SucursalController.php
+++ app/Http/Controllers/SucursalController.php
@@ -3,106 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\sucursal;
+
 
 class SucursalController extends Controller
 {
     public function ver(){
-        return $listar=sucursal::where('habilitado','habilitado')->get();
+        
     }
 
-    public function insertar(Request $request){
-        $json=$request->input('json',null);
-        $params=json_decode($json);
-        
-        $nombre	=(!is_null($json) && isset($params->nombre)) ? $params->nombre : null;
-        $descripcion=(!is_null($json) && isset($params->descripcion)) ? $params->descripcion : null;
-        $direccion=(!is_null($json) && isset($params->direccion)) ? $params->direccion : null;
-        $telefono=(!is_null($json) && isset($params->telefono)) ? $params->telefono : null;
-        $id_almacen=(!is_null($json) && isset($params->id_almacen)) ? $params->id_almacen : null;
-        $id_user=(!is_null($json) && isset($params->id_user)) ? $params->id_user : null;
-
-        if(!is_null($nombre)){
-            $habilitado='habilitado';
-            $sucursal=new sucursal();
-
-            $sucursal->nombre=$nombre;
-            $sucursal->descripcion=$descripcion;
-            $sucursal->direccion=$direccion;
-            $sucursal->telefono=$telefono;
-            $sucursal->id_user=$id_user;
-            $sucursal->id_almacen=$id_almacen;
-            $sucursal->habilitado=$habilitado;
-            
-
-            $isset_cate=sucursal::where('nombre','=',$nombre)->first();
-            if(@count($isset_cate)==0){
-                //guardar
-                $sucursal->save();
-
-                $data =array(
-                    'status'=>'succes',
-                    'code'=>200,
-                    'mensage'=>'registrado'
-                );
-            }else{
-                //no guiardar
-                $data =array(
-                    'status'=>'error',
-                    'code'=>300,
-                    'mensage'=>'ya existe'
-                );
-            }
-        
-        }else{
-            $data =array(
-                'status'=>'error',
-                'code'=>400,
-                'mensage'=>'faltan datos'
-            );
-        }
-        return response()->json($data,200);
-        /*$categoria=Categoria::create($request->all());
-        return $categoria;*/
+    public function addSucursal(Request $request){
+        $create=Sucursal::create($request->all());
+        return response()->json($create);
     }
        
-    public function modificar($id,Request $request){
-        $json=$request->input('json',null);
-        $params=json_decode($json);
-        
-        $nombre	=(!is_null($json) && isset($params->nombre)) ? $params->nombre : null;
-        $descripcion=(!is_null($json) && isset($params->descripcion)) ? $params->descripcion : null;
-        $direccion=(!is_null($json) && isset($params->direccion)) ? $params->direccion : null;
-        $telefono=(!is_null($json) && isset($params->telefono)) ? $params->telefono : null;
-        $id_almacen=(!is_null($json) && isset($params->id_almacen)) ? $params->id_almacen : null;
-        $id_user=(!is_null($json) && isset($params->id_user)) ? $params->id_user : null;
-              //guardar
-                $sucursal= sucursal::where('id',$id)->update(['nombre'=>$nombre,
-                'descripcion'=>$descripcion,
-                'direccion'=>$direccion,
-                'telefono'=>$telefono,
-                'id_almacen'=>$id_almacen,
-                'id_user'=>$id_user]);
+    public function updateSucursal($id,Request $request){
+        $edit=Sucursal::find($id)->update($request->all());
+    }
 
-                $data =array(
-                    'status'=>'succes',
-                    'code'=>200,
-                    'mensage'=>'registrado'
-                );
-            
-        return response()->json($data,200);
-
-       }
-
-       public function seleccionar($id){
-       
-        $sucursal=sucursal::find($id);
-        return $sucursal;
-       }
+   public function seleccionar($id){
+   
+   }
 
     public function eliminar($id){
-        $cambio='desabilitado';
-        $sucursal=sucursal::where('id',$id)->update(['habilitado'=>$cambio]);
-    	return $sucursal;
-       }
+        
+    }
 }
--- app/sucursal.php
+++ app/sucursal.php
@@ -4,7 +4,8 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class sucursal extends Model
+class Sucursal extends Model
 {
-    //
+   protected $table='sucursals';
+   protected $fillable=['nombre','direccion','telefono','id_almacen','descripcion','estado','id_user'];
 }
