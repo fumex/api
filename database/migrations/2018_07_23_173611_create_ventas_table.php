80adf2b4e5339da65e1155b6abbade7eabd961b7
miguel angel <migueljeaguer@gmail.com>
2018-07-24 10:31:11 -0500

    ventas

---
+++ database/migrations/2018_07_23_173611_create_ventas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVentasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ventas', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('tipo_venta');
+            $table->integer('id_caja');
+            $table->Decimal('total');
+            $table->Decimal('pago_efectivo');
+            $table->Decimal('pago_targeta');
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ventas');
+    }
+}
